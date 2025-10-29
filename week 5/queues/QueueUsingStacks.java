9ed07d0f7cde30438d90427b25ad028a2caf2ebc
syrnxalno <archanakrishna0023@gmail.com>
2025-10-29 21:13:04 +0530

    queue using stacks

---
+++ week 5/queues/QueueUsingStacks.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+class QueueUsingStacks {
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
+
+    void enqueue(int x) {
+        s1.push(x);
+    }
+
+    int dequeue() {
+        if (s2.isEmpty()) {
+            if (s1.isEmpty()) {
+                return -1;
+            }
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.pop();
+    }
+
+    // every other operation remains pretty much the same
+
+    public static void main(String[] args) {
+        QueueUsingStacks q = new QueueUsingStacks();
+        q.enqueue(10);
+        q.enqueue(20);
+        q.enqueue(30);
+        System.out.println(q.dequeue()); 
+        System.out.println(q.dequeue()); 
+    }
+}
