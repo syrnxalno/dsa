b05cdc4cd44727c62af54f978dfd3e5eeb06d883
syrnxalno <archanakrishna0023@gmail.com>
2025-11-04 20:26:47 +0530

    sorting algorithms using recursion

--- week 6/Sorting.java
+++ week 6/Sorting.java
@@ -9,7 +9,7 @@ public class Sorting {
         for (int i = 0; i < n - 1; i++)
             if (arr[i] > arr[i + 1]) {
                 int temp = arr[i];
-                arr[i] = arr[i + 1];
+                arr[i] = arr[i +1];
                 arr[i + 1] = temp;
                 count = count + 1;
             }
